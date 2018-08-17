64da739fb346109cfb00ffb704d8cd71b4f98ee7
SC-201806121637\Administrator <596534985@qq.com>
2018-08-17 18:46:47 +0800

    地址

--- src/store/store.js
+++ src/store/store.js
@@ -4,8 +4,8 @@ Vue.use(Vuex);
 
 const state = {
     token: '',
-    // baseUrl: 'http://192.168.20.158:8080/',
-     baseUrl: 'http://www.scdiaoyu.cn/fish/',
+    baseUrl: 'http://192.168.20.158:8080/',
+    //  baseUrl: 'http://www.scdiaoyu.cn/fish/',
     imagesUrl:'http://www.scdiaoyu.cn/fish/common/uploadOssPic',
 };
 export default new Vuex.Store({
