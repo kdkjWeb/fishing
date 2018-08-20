3409a5b9c322cfee35bf8b87295721fb59ae595e
SC-201806121637\Administrator <596534985@qq.com>
2018-08-20 18:33:38 +0800

    bug

--- src/store/store.js
+++ src/store/store.js
@@ -6,7 +6,8 @@ const state = {
     token: '',
     baseUrl: 'http://192.168.20.158:8080/',
     //  baseUrl: 'http://www.scdiaoyu.cn/fish/',
-    imagesUrl:'http://www.scdiaoyu.cn/fish/common/uploadOssPic',
+    // imagesUrl:'http://www.scdiaoyu.cn/fish/common/uploadOssPic',
+  imagesUrl: 'http://192.168.20.158:8080/common/uploadOssPic',
 };
 export default new Vuex.Store({
     state: state,
